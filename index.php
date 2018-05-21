f18997c2e7c13144eed6ad48a9aa36619d6b623a
Fernando Meneses Poveda <fernando.meneses@adsalsa.com>
2018-05-21 12:41:41 +0200

    Sync responsive canvas + camera aspect

--- index.php
+++ index.php
@@ -1,126 +1,194 @@
 <!DOCTYPE html>
-	<html>
+<html>
 
-	<head>
-		<script src="js/three.js-master/build/three.js"></script>
-		<script src="js/three.js-master/examples/js/loaders/OBJLoader.js"></script>
-		<script src="js/three.js-master/examples/js/controls/OrbitControls.js"></script>
-		<title>My first three.js app</title>
-		<style>
-			body {
-				margin: 0;
-			}
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/three.js-master/build/three.js"></script>
+	<script src="js/three.js-master/examples/js/loaders/OBJLoader.js"></script>
+	<script src="js/three.js-master/examples/js/controls/OrbitControls.js"></script>
 
-			canvas {
-				width: 100%;
-				height: 100%
-			}
-		</style>
-	</head>
 
-	<body>
-		<script>
-			var container;
-			var camera, scene, renderer, orbit;
-			var mouseX = 0, mouseY = 0;
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
+	<link type="text/css" src="css/normalize.css" />
+	<link type="text/css" src="css/skeleton.css" />
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-			init();
-			animate();
+	<title>My first three.js app</title>
+	<style>
+		body {
+			font-family: 'Roboto', sans-serif;
+			font-weight: 100;
+			margin: auto;
+			padding: 0;
+		}
 
-			function init() {
-				
-				container = document.createElement('div');
-				document.body.appendChild(container);
+		.configurator {
+			background: url(img/bg-cars-home.jpg) no-repeat;
+			background-repeat: no-repeat;
+			background-size: 100% 100%;
+		}
 
-				camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-				camera.position.z = 200 //scene
-				camera.position.y = 60 //scene
+		canvas {
+			width: 100%;
+			max-width: 1024px;
+			height: 100%;
+			max-height: 400px;
+		}
 
-				scene = new THREE.Scene();
-				
-				var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
-				scene.add(ambientLight);
+		.max-width {
+			max-width: 1024px;
+			margin: auto;
+		}
 
-				var pointLight = new THREE.PointLight(0xffffff, 0.8);
-				camera.add(pointLight);
+		.container.title h1 {
+			padding: 1.5rem 0;
+			background-color: #041a2f;
+			font-size: 2.5rem;
+			margin: auto;
+			text-align: center;
+			color: white;
+			line-height: 1.2;
+    		letter-spacing: -.1rem;
+		}
 
-				scene.add(camera); // texture
+		.title a {
+			text-decoration: none;
+			cursor: inherit;
+			font-weight: 600;
+		}
+	</style>
+</head>
 
-				
-				
-				var color = new THREE.Color("rgb(171%, 175%, 180%)");
-				scene.background = color;
+<body>
 
-				var manager = new THREE.LoadingManager();
-				manager.onProgress = function (item, loaded, total) {
-					console.log(item, loaded, total);
-				};
+	<div class="container title">
+		<div class="row">
+			<div class="columns twelve">
+				<h1>LA NUOVA
+					<a>FIAT 500</a> PUÒ ESSERE
+					<a>TUA!</a>
+				</h1>
+			</div>
+		</div>
+	</div>
 
-				var textureLoader = new THREE.TextureLoader(manager);
-				var texture = textureLoader.load('js/three.js-master/examples/textures/UV_Grid_Sm.jpg'); // model
-				var onProgress = function (xhr) {
-					if (xhr.lengthComputable) {
-						var percentComplete = xhr.loaded / xhr.total * 100;
-						console.log(Math.round(percentComplete, 2) + '% downloaded');
+	<div class="container max-width">
+		<div class="container configurator">
+		</div>
+	</div>
+
+
+
+
+	<script>
+		var container;
+		var camera, scene, renderer, orbit;
+		var mouseX = 0,
+			mouseY = 0;
+		var windowHalfX = jQuery(".configurator").width() / 2;
+		var windowHalfY = jQuery(".configurator").height() / 2;
+
+		init();
+		animate();
+
+		function init() {
+
+			container = document.createElement('div');
+			jQuery(".configurator").append(container);
+
+			camera = new THREE.PerspectiveCamera(45, 1024 / 400, 1, 1000);
+
+			scene = new THREE.Scene();
+
+			var ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+			scene.add(ambientLight);
+
+			var pointLight = new THREE.PointLight(0xffffff, 0.8);
+			camera.add(pointLight);
+
+			scene.add(camera); // texture
+
+		
+			scene.background = null;
+
+			var manager = new THREE.LoadingManager();
+			manager.onProgress = function (item, loaded, total) {
+				console.log(item, loaded, total);
+			};
+
+			var textureLoader = new THREE.TextureLoader(manager);
+			var texture = textureLoader.load('js/three.js-master/examples/textures/UV_Grid_Sm.jpg'); // model
+			var onProgress = function (xhr) {
+				if (xhr.lengthComputable) {
+					var percentComplete = xhr.loaded / xhr.total * 100;
+					console.log(Math.round(percentComplete, 2) + '% downloaded');
+				}
+			};
+			var onError = function (xhr) {};
+			var loader = new THREE.OBJLoader(manager);
+			loader.load('obj/C0818121.obj', function (object) {
+				object.traverse(function (child) {
+					if (child instanceof THREE.Mesh) {
+						child.material.map = texture;
 					}
-				};
-				var onError = function (xhr) {};
-				var loader = new THREE.OBJLoader(manager);
-				loader.load('obj/C0818121.obj', function (object) {
-					object.traverse(function (child) {
-						if (child instanceof THREE.Mesh) {
-							child.material.map = texture;
-						}
-					});
-					object.position.y = 0;
-					
-					scene.add(object);
-				}, onProgress, onError);
+				});
+				object.position.y = -15;
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio(window.devicePixelRatio);
-				renderer.setSize(window.innerWidth, window.innerHeight);
-				container.appendChild(renderer.domElement);
-				
-				window.addEventListener('resize', onWindowResize, false);
+				scene.add(object);
+			}, onProgress, onError);
 
+			renderer = new THREE.WebGLRenderer({ alpha: true });
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			container.appendChild(renderer.domElement);
 
-				//Camara Orbit
-				orbit = new THREE.OrbitControls(camera, renderer.domElement);
-				orbit.target = new THREE.Vector3(0,0,0); // set the center
-				orbit.rotateSpeed = 0.3;
-				orbit.autoRotate = true;
-				orbit.enableZoom = true;
-				orbit.maxDistance = 200;
+			renderer.setClearColor( 0x000000, 0 ); 
 
-						
-				
+			window.addEventListener('resize', onWindowResize, false);
 
-			}
-
-				function onWindowResize() {
-					windowHalfX = window.innerWidth / 2;
-					windowHalfY = window.innerHeight / 2;
-					camera.aspect = window.innerWidth / window.innerHeight;
-					camera.updateProjectionMatrix();
-					renderer.setSize(window.innerWidth, window.innerHeight);
-				}
-
-				function animate() {
-					requestAnimationFrame(animate);
-					render();
-				}
-
-				function render() {
-					orbit.update();
-					camera.lookAt(scene.position);
-					renderer.render(scene, camera);
-				}
-
+			//Camara Orbit
 			
-		</script>
-	</body>
+			orbit = new THREE.OrbitControls(camera, renderer.domElement);
+			orbit.target = new THREE.Vector3(0, 0, 0); // set the center
+			orbit.rotateSpeed = 0.3;
+			
+			orbit.enableZoom = true;
+			//Fix Rotate only move X
+			orbit.maxDistance = 120;
+			orbit.minDistance = 120;
+			orbit.maxPolarAngle = 1.4;
+			orbit.minPolarAngle = 1.4;
+		}
 
-	</html>
+		function onWindowResize() {
+			windowHalfX = jQuery(".configurator").width() / 2;
+			windowHalfY = jQuery(".configurator").height() / 2;
+			camera.aspect = jQuery(".configurator").width() / jQuery(".configurator").height();
+			camera.updateProjectionMatrix();
+			renderer.setSize(jQuery(".configurator").width(), jQuery(".configurator").height());
+		}
+
+		function animate() {
+			requestAnimationFrame(animate);
+			render();
+		}
+
+		function StartAutoPlay(){
+			orbit.autoRotate = true;
+		}
+
+		function StopAutoPlay(){
+			orbit.autoRotate = true;
+		}
+
+		function render() {
+			orbit.update();
+			camera.lookAt(scene.position);
+			renderer.render(scene, camera);
+		}
+	</script>
+</body>
+
+</html>
